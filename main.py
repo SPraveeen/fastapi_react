abf010b8e5aa1b99dcff2bf917221e046ec0282e
SPraveeen <praveenji8498@gmail.com>
2025-05-31 11:35:58 +0530

    bug

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ models.Base.metadata.create_all(bind=engine)
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/tasks/{task_id}")
+def read_task(task_id: int):
+    return {"task_id": task_id,"name": name,"description": description,"status": status}
+
+@app.post("/tasks/")
+def create_task():
+    return {"task_id": task_id,"name": name,"description": description,"status": status}
+    
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int):
+    return {"task_id": task_id,"name": name,"description": description,"status": status}
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    return {"task_id": task_id,"name": name,"description": description,"status": status}
